d7222773c892194a05039e5ec1b845174b4da97f
Alexdevsoft <alexhavilla2022@gmail.com>
2022-07-11 20:01:22 -0300

    Other componets

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assests/img/logo.svg'
+
+import './style.css'
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="dsmeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://github.com/Alexdevsoft">Alexsandro Almeida</a>
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
